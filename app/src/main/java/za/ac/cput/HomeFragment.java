af96938aa01c0912077382cbfc0760894cc216c3
momo-github788 <momoZzzz1996@gmail.com>
2023-03-24 10:27:19 +0200

    Added sign up layout, donate points layout, home layout, navigation menu

---
+++ app/src/main/java/za/ac/cput/HomeFragment.java
@@ -0,0 +1,81 @@
+package za.ac.cput;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
+
+import za.ac.cput.domain.Objective;
+
+public class HomeFragment extends Fragment implements View.OnClickListener{
+
+
+    private Button buyPointsBtn, pointsHistoryBtn;
+    private RecyclerView objectiveRecyclerView;
+    private ObjectivesRecyclerAdapter objectivesRecyclerAdapter;
+
+    private List<Objective> objectiveList = List.of(
+            new Objective("Donate points", "Donate points to a friend", 250, false),
+            new Objective("Add a bank card", "Add a bank card to your account", 250, false),
+            new Objective("Spend 5000 points", "Spend your first 5000 points", 250, true),
+            new Objective("Spend 10 000 points", "Spend your first 10 000 points", 250, false),
+            new Objective("Spend 10 000 points", "Spend your first 10 000 points", 250, true),
+            new Objective("Spend 10 000 points", "Spend your first 10 000 points", 250, false)
+    );
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, null);
+
+        buyPointsBtn = view.findViewById(R.id.buyPointsBtn);
+        pointsHistoryBtn = view.findViewById(R.id.pointsHistoryBtn);
+
+        buyPointsBtn.setOnClickListener(this);
+
+        buildRecyclerView(view);
+        return view;
+    }
+
+
+    private void buildRecyclerView(View v) {
+        objectiveRecyclerView = v.findViewById(R.id.objectiveRecyclerView);
+        objectiveRecyclerView.setHasFixedSize(true);
+        objectiveRecyclerView.setLayoutManager(new LinearLayoutManager(HomeFragment.this.getActivity()));
+        objectivesRecyclerAdapter = new ObjectivesRecyclerAdapter(HomeFragment.this.getActivity(), objectiveList, null);
+        objectiveRecyclerView.setAdapter(objectivesRecyclerAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buyPointsBtn) {
+           replaceFragment(new DonatePointsFragment());
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getActivity().getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+}
--- app/src/main/java/za/ac/cput/MainActivity.java
+++ app/src/main/java/za/ac/cput/MainActivity.java
@@ -1,14 +1,90 @@
 package za.ac.cput;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialToolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        replaceFragment(new HomeFragment());
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.materialToolBar);
+
+        // open navigation drawer layout when clicking icon in toolbar
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        // handle item click in navigation view
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                item.setChecked(true);
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id) {
+                    case R.id.logoutNavMenu:
+                        //replaceFragment(new SignUpFragment());
+                        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                        break;
+
+                    case R.id.donatePointsNavMenu:
+                        replaceFragment(new DonatePointsFragment());
+                        break;
+
+                    case R.id.viewPointsNavMenu:
+                        replaceFragment(new HomeFragment());
+                        break;
+
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+
+
 }
